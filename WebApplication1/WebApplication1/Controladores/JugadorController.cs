e3e632d84484530335d5b4335643f4278a8ead48
Erwin <erwin.paralesda@gmail.com>
2024-02-08 16:58:00 -0500

    Add files via upload

---
+++ WebApplication1/WebApplication1/Controladores/JugadorController.cs
@@ -0,0 +1,53 @@
+﻿using System.Web.Mvc;
+using CRUD.Modelos;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CRUD.Controladores
+{
+    public class JugadorController : Controller
+    {
+
+        private CRUDContext context = null;
+
+        public JugadorController() {
+
+            context = new CRUDContext();
+        }
+
+        // GET: Jugador
+        public JsonResult GetJugador()
+        {
+            List<Jugador> jugadorLista = context.Jugador.ToList();
+            return Json(new { List = jugadorLista }, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetJugadorId(int id)
+        {
+            Jugador jugador = context.Jugador.Where(c => c.JugadorID == id).SingleOrDefault();
+            return Json(new { jugador = jugador}, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult AgregarJugador(Jugador jugador) {
+
+            context.Jugador.Add(jugador);
+            context.SaveChanges();
+            return Json(new { status = "Jugador Insertado Correctamente!!!" });
+        }
+
+        public JsonResult ActualizarJugador(Jugador jugador)
+        {
+            context.Entry(jugador).State = System.Data.Entity.EntityState.Modified;
+            context.SaveChanges();
+            return Json(new { status = "Jugador Actualizado Correctamente!!!" });
+        }
+
+        public JsonResult EliminarJugador(int id) {
+
+            Jugador jugador = context.Jugador.Where(c => c.JugadorID == id).SingleOrDefault();
+            context.Jugador.Remove(jugador);
+            context.SaveChanges();
+            return Json(new { status = "Jugador Eliminado Correctamente!!!" });
+        }
+    }
+}
